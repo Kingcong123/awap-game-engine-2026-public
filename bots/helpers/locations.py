0d763a4cb1514a2e13a80716fa70acb5f56bcb92
Kingcong123 <congw0609@gmail.com>
2026-01-31 15:23:20 -0500

    locations fixed

--- bots/helpers/locations.py
+++ bots/helpers/locations.py
@@ -1,12 +1,12 @@
 
 def find_important_locations(map_instance):
         locations = {
-            "COOKER": None,           # 'K'
-            "SINK": None,             # 'S'
-            "SINKTABLE": None,        # 'T'
-            "SUBMIT": None,           # 'U'
-            "SHOP": None,             # '$'
-            "TRASH": None,            # 'R'
+            "COOKER": [],           # 'K'
+            "SINK": [],             # 'S'
+            "SINKTABLE": [],        # 'T'
+            "SUBMIT": [],           # 'U'
+            "SHOP": [],             # '$'
+            "TRASH": [],            # 'R'
             "COUNTER": [],          # 'C'
         }
 
@@ -16,10 +16,7 @@ def find_important_locations(map_instance):
                 tile_name = map_instance.tiles[x][y].tile_name
                 
                 # Map the exact tile names to our dictionary keys
-                if tile_name == "COUNTER":
+                if tile_name in locations:
                     locations[tile_name].append((x, y))
-                else:
-                    if tile_name in locations:
-                        locations[tile_name] = (x, y)
 
         return locations
